cada184d75c0734ec89f3b3830ca756001f3d970
Martin Walther <martin.walther@swisscom.com>
2019-05-29 20:39:14 +0200

    Play with format

--- js/search.js
+++ js/search.js
@@ -7,7 +7,7 @@
   
         for (var i = 0; i < results.length; i++) {  // Iterate over the results
           var item = store[results[i].ref];
-          appendString += '<li><a href="' + item.url + '"><h1>' + item.title + '</h1></a>';
+          appendString += '<li><a href="' + item.url + '"><h2>' + item.title + '</h2></a>';
           appendString += '<p>' + item.content.substring(37, 350) + '...</p></li>';
           appendString += '<p><b>' + item.category + '</b></p></li>';
         }
